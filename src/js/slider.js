4311175400643bcc0d78e69899e6bcaf249ccd17
Dan Riches <driches@ymail.com>
2018-05-05 14:52:56 -0700

    redid the slideshow

---
+++ src/js/slider.js
@@ -0,0 +1,18 @@
+var slideIndex = 0;
+
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "flex";
+    setTimeout(showSlides, 5000);
+}
+
+showSlides();
